0f4417c9991c1a9ff68633be5728481a7bd25b49
Jesa <jesa.setiadi@yahoo.com>
2019-01-28 16:48:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ManageProduk from './components/ManageProduk';
+import HomeBertasbih from './components/HomeBertasbih';
+import MovieList from './components/MovieList';
+import CategoryList from './components/CategoryList';
+import ConnectionList from './components/ConnectionList';
+// import { Route } from 'react-router-dom';
+// import { connect } from 'react-redux';
+// import { withRouter} from 'react-router-dom';
+
 
 class App extends Component {
+  state = { content: 'Ini Home Page'}
   render() {
     return (
       <div className="App">
-        <ManageProduk />
+        {/* <HomeBertasbih /> */}
+        <MovieList />
+        {/* <CategoryList />
+        <ConnectionList /> */}
       </div>
     );
   }
--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,26 +1,23 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-class ManageProduk extends Component{
+class CategoryList extends Component{
     state = {listProduct: [], selectedEditProductId: 0}
 
     componentDidMount(){
-        axios.get('http://localhost:1999/product')
+        axios.get('http://localhost:1997/categorylist')
         .then((res)=>{
         this.setState({listProduct:res.data})
         })
     }
 
     onBtnAddClick = () => {
-        axios.post('http://localhost:1999/addproduct', {
+        axios.post('http://localhost:1997/addcategory', {
             nama: this.refs.AddNama.value,
-            harga: this.refs.AddHarga.value,
-            deskripsi: this.refs.AddDesc.value,
-            image: this.refs.AddImg.value,
-            namabrand: this.refs.AddBrand.value
+        
         })
         .then((res) => {
-            alert("Add Brand Success")
+            alert("Add Category Success")
             this.setState({listProduct:res.data})
         })
         .catch((err) =>{
@@ -30,7 +27,7 @@ class ManageProduk extends Component{
 
     onBtnDeleteClick = (id) => {
         if(window.confirm('Are you sure to delete?')) {
-            axios.delete('http://localhost:1999/deleteproduct/' + id)
+            axios.delete('http://localhost:1997/deletecategory/' + id)
             .then((res) => {
                 alert('Delete Success');
                 this.setState({ listProduct: res.data })
@@ -42,25 +39,18 @@ class ManageProduk extends Component{
         }
     }
 
-
     onBtnSaveClick = (id) => {
-        axios.put('http://localhost:1999/editproduct/' + id, {
-            nama: this.refs.EditNama.value,
-            harga: this.refs.EditHarga.value,
-            deskripsi: this.refs.EditDesc.value,
-            image: this.refs.EditImg.value,
-            namabrand: this.refs.EditBrand.value
+        axios.put('http://localhost:1997/editcategory/' + id, {
+            nama: this.refs.EditNama.value
         })
         .then((res) => {
-            alert("Edit Brand Success")
+            alert("Edit Category Success")
             this.setState({ listProduct: res.data, selectedEditProductId: 0 })
         })
         .catch((err) =>{
             console.log(err)
         })
     }
-
-
 
     renderProductList = () => {
         var listJSX = this.state.listProduct.map((item) => {
@@ -69,10 +59,6 @@ class ManageProduk extends Component{
                     <tr>
                         <td></td>
                         <td><input type="text" ref="EditNama" defaultValue={item.nama} /></td>
-                        <td><input type="text" ref="EditHarga" defaultValue={item.harga} /></td>
-                        <td><input type="text" ref="EditDesc" defaultValue={item.deskripsi} /></td>
-                        <td><input type="text" ref="EditImg" defaultValue={item.image} /></td>
-                        <td><input type="text" ref="EditBrand" defaultValue={item.namabrand} /></td>
                         <td><input type="button" class="btn btn-primary" value="Cancel" onClick={() => this.setState({ selectedEditProductId: 0 })} /></td>
                         <td><input type="button" class="btn btn-primary" value="Save" onClick={() => this.onBtnSaveClick(item.id)} /></td>
                     </tr>
@@ -82,10 +68,6 @@ class ManageProduk extends Component{
                 <tr>
                     <td>{item.id}</td>
                     <td>{item.nama}</td>
-                    <td>{item.harga}</td>
-                    <td>{item.deskripsi}</td>
-                    <td>{item.image}</td>
-                    <td>{item.namabrand}</td>
                     <td><input type="button" class="btn btn-primary" value="Edit" onClick={() => this.setState({selectedEditProductId:item.id})} /></td>
                     <td><input type="button" class="btn btn-danger" value="Delete" onClick={() => this.onBtnDeleteClick(item.id)} /></td>
                 </tr>
@@ -98,16 +80,12 @@ class ManageProduk extends Component{
         return(
             <div>
             <center>
-                <h1>Product List</h1>
+                <h1>Category List</h1>
                 <table>
                     <thead>
                         <tr>
-                            <th>ID</th>
-                            <th>Nama Produk</th>
-                            <th>Harga</th>
-                            <th>Deskripsi</th>
-                            <th>Image</th>
-                            <th>Brand</th>
+                            <th>Id</th>
+                            <th>Nama</th>
                             <th></th>
                             <th></th>
                         </tr>
@@ -118,11 +96,7 @@ class ManageProduk extends Component{
                     <tfoot>
                         <tr>
                             <td></td>
-                            <td><input type="text" ref="AddNama" placeholder="Masukan Nama Product"/></td>
-                            <td><input type="text" ref="AddHarga" placeholder="Masukan Harga"/></td>
-                            <td><input type="text" ref="AddDesc" placeholder="Masukan Deskripsi"/></td>
-                            <td><input type="text" ref="AddImg" placeholder="Masukan Image" /></td>
-                            <td><input type="text" ref="AddBrand" placeholder="Masukan Brand" /></td>
+                            <td><input type="text" ref="AddNama" placeholder="Masukan Nama Category"/></td>
                             <td></td>
                             <td><input type="button" class="btn btn-success" value="Add" onClick={this.onBtnAddClick} /></td>
                         </tr>
@@ -134,4 +108,4 @@ class ManageProduk extends Component{
     }
 }
 
-export default ManageProduk;
+export default CategoryList;
